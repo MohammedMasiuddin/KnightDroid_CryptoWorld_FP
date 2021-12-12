6fbd8a5b62a1c0bad9b8611cd0a2a3bd9f8e5904
masiuddinC0796887 <c0796887@mylambton.ca>
2021-12-12 11:21:18 -0400

    Home page structure done

--- javascript/indexpage.js
+++ javascript/indexpage.js
@@ -1,6 +1,22 @@
-(function () {
+$(document).ready(function () {
     
-    window.onload = 
-    
+    $.ajax({
+        type: "get",
+        url: "https://api.coinlore.net/api/global/",
+        data: "",
+        dataType: "json",
+        success: function (response) {
+            $("#Cryptocurrenciescount span").html(response[0].coins_count);
+            $("#crptMarket span").html(response[0].active_markets);
+            $("#marketcap span").html(response[0].total_mcap);
+            $("#volumenum span").html(response[0].total_volume);
+            $("#btcdom span").html(response[0].btc_d + "%");
+            $("#ethdom span").html(response[0].eth_d + "%");
+            $("#mcapchange span").html(response[0].mcap_change + "%");
+            $("#volchange span").html(response[0].volume_change + "%");
+            $("#avgchange span").html(response[0].avg_change_percent + "%");
 
-})()
+        }
+    });
+
+});
