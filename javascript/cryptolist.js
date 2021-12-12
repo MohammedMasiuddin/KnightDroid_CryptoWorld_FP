41162494309f9047c906780e0cd502439065f531
masiuddinC0796887 <c0796887@mylambton.ca>
2021-12-12 15:35:37 -0400

    cypto list getting data and images

--- javascript/cryptolist.js
+++ javascript/cryptolist.js
@@ -1,7 +1,7 @@
 $(function () {
     var datalistcount = 0;
     addmore(datalistcount);
-    // $("#crpto").remove();
+    $("#crpto").remove();
 
     $("#loadmore").on("click", function () {
         datalistcount = datalistcount+12;
@@ -19,12 +19,18 @@ $(function () {
     
                 $.each(response.data, function (indexInArray, valueOfElement) {             
                     $(".container").append(
-                       ` <div id="crpto"  class="childcrypto" >
-                    <p id="symbol" >${valueOfElement.symbol}</p>
-                    <p id="name">${valueOfElement.name} </p>
-                    <p id="rank">${valueOfElement.rank} </p>
-                    <p id="price_usd"> $ ${valueOfElement.price_usd} </p>
-                </div>`
+                     
+                `
+                <div id="crpto" class="childcrypto">
+                    <img id="cryptoimage" src="https:www.coinlore.com/img/${valueOfElement.nameid}.png" alt="bitcoin">
+                    <div class="cardtext">
+                        <p id="symbol"><b>Symbol :</b>${valueOfElement.symbol}</p>
+                        <p id="name"> <b>Name :</b>${valueOfElement.name}</p>
+                        <p id="rank"> <b>Rank :</b>${valueOfElement.rank}</p>
+                        <p id="price_usd"><b>Price :</b>$ ${valueOfElement.price_usd}</p>
+                    </div>
+                </div>
+                `
                     );
                 });
             }
