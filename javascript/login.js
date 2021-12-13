1803396d4b3c934bd820ac03e24ce59fff192d9a
masiuddinC0796887 <c0796887@mylambton.ca>
2021-12-13 13:34:55 -0400

    index error resolved

--- javascript/login.js
+++ javascript/login.js
@@ -11,6 +11,46 @@ $(function () {
     });
 
 
+
+    
+    islogin()
+
+    function islogin(params) {
+        
+    var api_key = "AIzaSyAtHSQ6WaSr0XPEI9c9UTs0pokbwguB8RU"
+
+
+    var mystorage = window.localStorage;
+    mytoken = mystorage.getItem("refreshToken");
+
+    var somedata = {
+        "grant_type":"refresh_token",
+        "refresh_token":mytoken
+    }
+    $.ajax({
+        type: "POST",
+        url: `https://securetoken.googleapis.com/v1/token?key=${api_key}`,
+        data: `grant_type=refresh_token&refresh_token=${mytoken}`,
+        dataType: "json",
+        ContentType: `application/x-www-form-urlencoded`,
+        success: function (response) {
+            console.log(mytoken);
+            console.log("login");
+            console.log(response);
+            $(`.islogin`).show()
+            
+        },
+        error:function (error) {
+            console.log(error);
+            $(`.islogin`).hide()
+          }
+    });
+
+
+    }
+
+
+
     $(`#togglepaswd`).on('click', function (e) {
         console.log("sdfwreuihsdj");
 
