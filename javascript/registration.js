670762fd70224e55ace2bcef2e240ff8bb20569b
masiuddinC0796887 <c0796887@mylambton.ca>
2021-12-13 14:12:47 -0400

    some chnges

--- javascript/registration.js
+++ javascript/registration.js
@@ -1,6 +1,50 @@
 $(function () {
 
 
+    
+    islogin()
+
+    function islogin(params) {
+        
+    var api_key = "AIzaSyAtHSQ6WaSr0XPEI9c9UTs0pokbwguB8RU"
+
+
+    var mystorage = window.localStorage;
+    mytoken = mystorage.getItem("refreshToken");
+
+    var somedata = {
+        "grant_type":"refresh_token",
+        "refresh_token":mytoken
+    }
+    $.ajax({
+        type: "POST",
+        url: `https://securetoken.googleapis.com/v1/token?key=${api_key}`,
+        data: `grant_type=refresh_token&refresh_token=${mytoken}`,
+        dataType: "json",
+        ContentType: `application/x-www-form-urlencoded`,
+        success: function (response) {
+            console.log(mytoken);
+            console.log("login");
+            console.log(response);
+            $(`.islogin`).show()
+            // $(`.navlogin`).hide();
+            // $(`.navregister`).hide();
+            $(`#registorform`).html(" you are already login");
+            
+        },
+        error:function (error) {
+            console.log(error);
+            $(`.islogin`).hide();
+            // $(`.navlogin`).show();
+            // $(`.navregister`).show();
+          }
+    });
+
+
+    }
+
+
+
     $(`#togglepaswd`).on('click', function (e) {
         console.log("sdfwreuihsdj");
 
